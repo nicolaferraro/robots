d4dbba0750699eb1fb5a552a7e17d203405418a8
nicolaferraro <ni.ferraro@gmail.com>
2020-10-30 11:35:00 +0100

    21%

--- fusewarhw2020/TwiceInARow.java
+++ fusewarhw2020/TwiceInARow.java
@@ -2,7 +2,9 @@ package fusewarhw2020;
 
 import java.awt.*;
 import java.awt.geom.Point2D;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import robocode.AdvancedRobot;
@@ -27,8 +29,8 @@ public class TwiceInARow extends AdvancedRobot {
     private static final int MIN_BORDER_DISTANCE = 110;
     private static final int ABSOLUTE_STEP = 5000;
     private static final int RAMMING_DEVIATION_MIN_ENEMIES = 10;
-    private static final int RAMMING_DEVIATION_MIN = 0;
-    private static final int RAMMING_DEVIATION_MAX = 25;
+    private static final int RAMMING_DEVIATION_MIN = 5;
+    private static final int RAMMING_DEVIATION_MAX = 30;
     private static final int FIRE_BEARING_DISTANCE = 3;
     private static final double ENEMY_SPEED_OVERESTIMATE = 1.05;
     private static final double WORST_ENEMY_TOLERANCE = 0.25;
@@ -36,9 +38,13 @@ public class TwiceInARow extends AdvancedRobot {
     private static final double DEVIATION_FROM_CENTER_MULTIPLIER = 3.0;
     private static final double BULLET_SAVING = 1.0;
     private static final double BULLET_MIN_ATTEMPTS_ENERGY = 5.0;
+    private static final int INITIALIZATION_MAX_TIME = 10;
+    private static final double INITIALIZATION_MAX_DAMAGE = 2.9;
+    private static final double INITIALIZATION_MAX_ENERGY_LOSS = 5.0;
 
     private int direction = ABSOLUTE_STEP;
     private Map<String, RobotProfile> profiles = new HashMap<String, RobotProfile>();
+    private Double initialEnergy;
 
     public TwiceInARow() {
     }
@@ -119,6 +125,10 @@ public class TwiceInARow extends AdvancedRobot {
         if (e.isSentryRobot()) {
             return;
         }
+
+        if (initialize(e)) {
+            return;
+        }
         
         if(!isPersonalEnemy(e.getName())) {
             return;
@@ -153,6 +163,9 @@ public class TwiceInARow extends AdvancedRobot {
         double time = 0.0;
         while ((++iterations) <= maxIterations) {
             Point enemyPos = getFuturePoint(e, time);
+            if (Utils.isNear(time, 0.0)) {
+                p.lastLocation = enemyPos;
+            }
             double oldTime = time;
             time = Point2D.distance(getX(), getY(), enemyPos.getX(), enemyPos.getY()) / bulletSpeed;
             if (Utils.isNear(time, oldTime)) {
@@ -200,6 +213,98 @@ public class TwiceInARow extends AdvancedRobot {
             scan();
         }
     }
+
+    private boolean initialize(ScannedRobotEvent e) {
+        if (initialEnergy == null) {
+            initialEnergy = getEnergy();
+        }
+
+        if (getTime() > INITIALIZATION_MAX_TIME) {
+            return false;
+        }
+
+        if (getEnergy() <= initialEnergy - INITIALIZATION_MAX_ENERGY_LOSS) {
+            return false;
+        }
+
+        double sumDamage = 0;
+        for (RobotProfile p : this.profiles.values()) {
+            sumDamage += p.energyLoss;
+        }
+        if (sumDamage > INITIALIZATION_MAX_DAMAGE) {
+            return false;
+        }
+
+        if (DEBUG) {
+            System.out.println("Initialization phase (" + getTime() + " / " + INITIALIZATION_MAX_TIME + ")...");
+        }
+
+        RobotProfile sp = getProfile(e.getName());
+        Point l = getFuturePoint(e, 0.0);
+        sp.lastLocation = l;
+
+        List<Point> others = new ArrayList<>();
+        for (RobotProfile p : this.profiles.values()) {
+            if (p.dead || p.lastLocation == null) {
+                continue;
+            }
+            others.add(p.lastLocation);
+        }
+        if (others.size() == 0) {
+            others.add(new Point(getX(), getY()));
+        }
+
+        double sumx=0, sumy=0;
+        for (Point loc : others) {
+            sumx += loc.x;
+            sumy += loc.y;
+        }
+        Point center = new Point(sumx / others.size(), sumy / others.size());
+
+        double escapex, escapey;
+        if (center.x < getBattleFieldWidth() / 2) {
+            escapex = center.x + getBattleFieldWidth()/2;
+        } else {
+            escapex = center.x - getBattleFieldWidth()/2;
+        }
+        if (center.y < getBattleFieldHeight() / 2) {
+            escapey = center.y + getBattleFieldHeight()/2;
+        } else {
+            escapey = center.y - getBattleFieldHeight()/2;
+        }
+        
+        escapex = Math.min(Math.max(MIN_BORDER_DISTANCE, escapex), getBattleFieldWidth() - MIN_BORDER_DISTANCE);
+        escapey = Math.min(Math.max(MIN_BORDER_DISTANCE, escapey), getBattleFieldHeight() - MIN_BORDER_DISTANCE);
+        
+        double bearing = getBearingDegrees(escapex, escapey);
+        setTurnRight(bearing);
+        setTurnRadarRight(360);
+        if (DEBUG) {
+            double distance = Math.sqrt(Math.pow(getX() - escapex, 2) + Math.pow(getY() - escapey, 2));
+            System.out.println("Escape location x=" + escapex + ", y=" + escapey + ", distance=" + distance);
+        }
+
+        // Set closest target as next
+        RobotProfile nextTarget = null;
+        double nextTargetDistance = Double.MAX_VALUE;
+        for (RobotProfile p : this.profiles.values()) {
+            p.energyLoss = 0; // reset
+            if (p.lastLocation == null) {
+                continue;
+            }
+            double d = Math.sqrt(Math.pow(getX() - p.lastLocation.x, 2) + Math.pow(getY() - p.lastLocation.y, 2));
+            if (d < nextTargetDistance) {
+                nextTargetDistance = d;
+                nextTarget = p;
+            }
+        }
+        if (nextTarget != null) {
+            nextTarget.energyLoss=1.0;
+        }
+
+        return true;
+    }
+
 
     private Point getFuturePoint(ScannedRobotEvent e, double time) {
         double absoluteBearing = getHeadingRadians() + e.getBearingRadians();
@@ -309,6 +414,7 @@ public class TwiceInARow extends AdvancedRobot {
         private boolean dead;
         private double attemptsEnergy;
         private double hitsEnergy;
+        private Point lastLocation;
         
         private RobotProfile(String name) {
             this.name = name;
